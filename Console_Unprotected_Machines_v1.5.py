5417ac45fdb91e1a606d6ae390f932756c59be2e
Sophos Professional Services API Scripts <64469914+sophosukps@users.noreply.github.com>
2020-07-31 09:20:11 +0100

    Update Console_Unprotected_Machines_v1.5.py

--- Console_Unprotected_Machines_v1.5.py
+++ Console_Unprotected_Machines_v1.5.py
@@ -1,6 +1,30 @@
-# These scripts are examples and unsupported
-# Make sure requests and ldap3 are installed
-# Single Console Unprotected Machines v1.5
+# Copyright 2019-2020 Sophos Limited
+#
+# Licensed under the GNU General Public License v3.0(the "License"); you may
+# not use this file except in compliance with the License.
+#
+# You may obtain a copy of the License at:
+# https://www.gnu.org/licenses/gpl-3.0.en.html
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+# Console_Unprotected_Machines_v1.5.py
+#
+# Compares machines in Active Directory to machines in Sophos Central
+# Machines NOT in Sophos Central will be exported to a csv report
+#
+#
+# By: Michael Curtis and Robert Prechtel
+# Date: 29/5/2020
+# Version 1.5
+# README: This script is an unsupported solution provided by
+#           Sophos Professional Services
+
 import requests
 import csv
 import configparser
